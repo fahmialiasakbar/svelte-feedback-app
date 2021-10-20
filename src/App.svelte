ab548550b96971d3d7ff4ccdc675877d1e614a57
Fahmi Akbar <fahmialiasakbar@gmail.com>
2021-10-20 12:27:07 +0700

    Passing props, Foreach items

--- src/App.svelte
+++ src/App.svelte
@@ -1,66 +1,26 @@
 <script>
-	let firstName = 'Fahmi';
-	let lastName = 'Akbar';
-	let color = 'blue'
-	let showText = false
+	import FeedbackList from './components/FeedbackList.svelte'
 
-	$: name = firstName + ' ' + lastName
-
-	const toggle = () => {
-		color = color === 'blue' ? 'red' : 'blue'
-		showText = !showText
-		users = [...users, { id: '4', name: 'Charles'}]
-	}
-
-	let users = [
+	let feedback = [
 		{
-			id: '1',
-			name: 'Max'
+			id: 1,
+			rating: 10,
+			text: 'Lorem ipsum ai queas dela monzah dekiu aoi c oaiw oia woa oc vseda cawdawfvsv dwafsvsfe fawdaw eawad/'
 		},
 		{
-			id: '2',
-			name: 'Lando'
+			id: 2,
+			rating: 9,
+			text: 'Lorem ipsum ai queas dela monzah dekiu aoi c oaiw oia woa oc vseda cawdawfvsv dwafsvsfe fawdaw eawad/'
 		},
 		{
-			id: '3',
-			name: 'Carlos'
-		},
+			id: 3,
+			rating: 8,
+			text: 'Lorem ipsum ai queas dela monzah dekiu aoi c oaiw oia woa oc vseda cawdawfvsv dwafsvsfe fawdaw eawad/'
+		}
+
 	]
 </script>
 
 <main>
-	<h1 style="color: {color}">Hello {name}!</h1>
-	{#if showText}
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	{:else}
-	<p>No Text</p>
-	{/if}
-	<!-- <button on:click={() => color = 'purple'}>Click</button> -->
-	<button on:click={toggle}>Click</button>
-
-	{#each users as user (user.id)}
-		<h3>{user.id} : {user.name}</h3>
-	{/each}
+	<FeedbackList {feedback}/>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let feedback = []
+    console.log(feedback)
+</script>
+
+{#each feedback as fb(fb.id)}
+    <h3>{fb.text}</h3>
+{/each}
